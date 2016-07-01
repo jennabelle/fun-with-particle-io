ea5e5f5c0ffced09f7a8097ff07881c495e94693
Jenna Vuong <jennavuong@gmail.com>
2016-06-30 23:47:16 -0700

    center get device list button

--- src/components/app.js
+++ src/components/app.js
@@ -6,8 +6,10 @@ class App extends Component {
 		return (
 			<div className='app_background'>
 				<div className='app_header'></div>
-				Hi!
-				<button type="button" className="btn btn-primary get_device_list">Get Device List</button>
+
+				<div className="col-md-6 col-md-offset-3">
+					<button type="button" className="btn btn-primary get_device_list">Get Device List</button>
+				</div>
 			</div>
 		);
 	}
