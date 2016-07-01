9e302ab3dd77ca98ca17fbc849deee9e6abe96c6
Jenna Vuong <jennavuong@gmail.com>
2016-06-30 17:39:32 -0700

    add more response info

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import ReactDOM from 'react-dom';
 // ENDPOINTS
 // #1 list-devices
 // https://api.particle.io/v1/devices?access_token=d6576383889e1526c95853391923584b508071c4
-// device ids: 2a002c000a47343232363230 ('rssi' variable), 39003b000a47343232363230 ('n' variable)
+
+// device 1: 2a002c000a47343232363230
+// variable: 'rssi', functions: rand, len, ripcord
+
+// device 2: 39003b000a47343232363230
+// variable: 'n', functions: plus, times, reset, ripcord
 
 // #2 get-device-information
 // https://api.particle.io/v1/devices/2a002c000a47343232363230?access_token=d6576383889e1526c95853391923584b508071c4
