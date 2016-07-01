01c401a3972f894c889737659d4637d766c5281f
Jenna Vuong <jennavuong@gmail.com>
2016-06-30 17:10:37 -0700

    successfully retrieved info from endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// https://api.particle.io/v1/devices?access_token=d6576383889e1526c95853391923584b508071c4
+const access_token = 'd6576383889e1526c95853391923584b508071c4';
+
 const App = () => {
 	return <div>Hi!</div>;
 }
