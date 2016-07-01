e8f6c7c0c172894960e78b75813775a2d76a74d6
Jenna Vuong <jennavuong@gmail.com>
2016-07-01 00:14:28 -0700

    modified css, add axios package for GET and POST requests

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+	getDeviceList() {
+
+	}
 	render() {
 		return (
 			<div className='app_background'>
+
 				<div className='app_header'></div>
 
 				<div className="row">
 					<div className="col-md-6 col-md-offset-5">
-						<button type="button" className="btn btn-primary get_device_list">Get Device List</button>
+						<button type="button" className="btn btn-primary get_device_list" onClick={this.getDeviceList}>
+							Show My Devices
+						</button>
 					</div>
 				</div>
 			</div>
