06769989e02f444eb8ce17c693e121a0c849c080
Jenna Vuong <jennavuong@gmail.com>
2016-07-01 14:49:47 -0700

    refactored navBar into separate component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import NavBar from './navbar';
 
 class App extends Component {
 
@@ -9,9 +10,9 @@ class App extends Component {
 		this.state = {}
 	}
 	getDeviceList() {
-		axios.get(`https://api.particle.io/v1/devices?access_token=${this.props.access_token}`)
+		axios.get('https://api.particle.io/v1/devices?access_token=d6576383889e1526c95853391923584b508071c4')
 			.then(function(response) {
-				console.log('It worked!');
+				console.log('It worked! Response: ', response);
 			})
 			.catch(function(response) {
 				console.log('Hello!');
@@ -19,19 +20,22 @@ class App extends Component {
 	}
 	render() {
 		return (
-			<div className='app_background'>
 
-				<div className='app_header'></div>
+			<div>
+				<NavBar />
+				<div className='app_background'>
 
-				<div className="row">
-					<div className="col-md-6 col-md-offset-5">
-						<button type="button" className="btn btn-primary get_device_list" onClick={this.getDeviceList}>
-							Show My Devices
-						</button>
+					<div className='app_header'></div>
+
+					<div className="row">
+						<div className="col-md-6 col-md-offset-5">
+							<button type="button" className="btn btn-primary get_device_list" onClick={this.getDeviceList}>
+								Show My Devices
+							</button>
+						</div>
 					</div>
 				</div>
-			</div>
-		);
+			</div>);
 	}
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component  {
+
+	// constructor(props) {
+	// 	super();
+	// }
+	render() {
+		return (
+		<div className="navbar navbar-default navbar-fixed-top">
+	  		<div className="container">
+	  			<div className="navbar-header">
+	  				<a href="" className="navbar-brand">Welcome back, Jenna!</a>
+
+	  				<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	        			<span className="sr-only">Toggle navigation</span>
+	        			<span className="icon-bar"></span>
+	        			<span className="icon-bar"></span>
+	        			<span className="icon-bar"></span>
+	      			</button>
+	      		</div>
+	  			<div className="collapse navbar-collapse">
+	  				<ul className="nav navbar-nav navbar-right">
+	                    <li className="active"><a href="">Home</a></li>
+	  					<li><a href="">Log Out</a></li>
+	  				</ul>
+	  			</div>
+	  		</div>
+	  	</div>
+	  	);
+	}
+
+}
