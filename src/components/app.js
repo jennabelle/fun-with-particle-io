801cd70bbfc99330ed7c9822f9c43a1b68c4abd6
Jenna Vuong <jennavuong@gmail.com>
2016-06-30 23:52:21 -0700

    beautify get device list button

--- src/components/app.js
+++ src/components/app.js
@@ -7,8 +7,10 @@ class App extends Component {
 			<div className='app_background'>
 				<div className='app_header'></div>
 
-				<div className="col-md-6 col-md-offset-3">
-					<button type="button" className="btn btn-primary get_device_list">Get Device List</button>
+				<div className="row">
+					<div className="col-md-6 col-md-offset-5">
+						<button type="button" className="btn btn-primary get_device_list">Get Device List</button>
+					</div>
 				</div>
 			</div>
 		);
