ff549ad693714fd9209829415d820bed772b2ba6
Jenna Vuong <jennavuong@gmail.com>
2016-07-02 15:03:44 -0700

    fixed bug to render correct device info

--- src/components/ShowDeviceInfo.js
+++ src/components/ShowDeviceInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import _ from 'underscore';
 
 export default class ShowDeviceInfo extends Component {
 
@@ -10,7 +11,8 @@ export default class ShowDeviceInfo extends Component {
 			device_id: this.props.params.deviceId,
 			device_object: {}
 		}
-
+	}
+	componentWillMount() {
 		axios.get(`https://api.particle.io/v1/devices/${this.state.device_id}?access_token=d6576383889e1526c95853391923584b508071c4`)
 			.then( (response) => {
 					this.setState({
@@ -18,16 +20,21 @@ export default class ShowDeviceInfo extends Component {
 					});
 			})
 			.catch( (response) => {
-					console.log('show device info error! response: ', response.data)
+					console.log('show device info error! response: ', response)
 			})
+
 	}
 	render() {
 
 		console.log('inside render! this.state.device_object: ', this.state.device_object)
 
+		let currentDevice = this.state.device_object
+
 		return (
 			<div>
-				Hi!
+				<p>{ this.state.device_object.name }</p>
+				{ this.state.device_object.connected ? <p>{ this.state.device_object.connected.toString() }</p> : null }
+				<p>{ this.state.device_object.cellular }</p>
 			</div>
 			);
 	}
