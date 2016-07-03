7cd8f937d35dcdd421b9babfd17e7c9d1b4c2677
Jenna Vuong <jennavuong@gmail.com>
2016-07-03 12:57:15 -0700

    change nar bar css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,10 +5,13 @@ export default class NavBar extends Component  {
 	
 	render() {
 		return (
-		<div className="navbar navbar-default navbar-fixed-top">
+		<div className="navbar navbar-inverse navbar-fixed-top navBarStyle">
 	  		<div className="container">
 	  			<div className="navbar-header">
-	  				<Link to="/" className="navbar-brand">Welcome back, Jenna!</Link>
+	  				<span>
+	  				<Link to="/" className="navbar-brand">
+	  					<img src="http://punchcard.io/wp-content/uploads/2016/04/icon-particle.svg" className="icon-particle" />
+	  				</Link></span>
 
 	  				<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 	        			<span className="sr-only">Toggle navigation</span>
