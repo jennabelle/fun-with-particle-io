c8b3081085c7ad7263bca3eeb8ca9b2d7996c84f
Jenna Vuong <jennavuong@gmail.com>
2016-07-03 17:56:19 -0700

    add unit tests, modify unit test helper

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,30 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+// import { renderComponent , expect } from '../test_helper'
+import React from 'react';
+import { 
+	renderIntoDocument,
+	scryRenderedDOMComponentsWithTag,
+	Simulate
+	 } from 'react-addons-test-utils'
+import { expect } from 'chai'
+import App from '../../src/components/app'
 
 describe('App' , () => {
   let component;
 
   beforeEach(() => {
-    component = renderComponent(App);
+    component = renderIntoDocument(<App />);
   });
 
-  it('renders something', () => {
+  it('renders two buttons', () => {
+  	const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
     expect(component).to.exist;
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].textContent).to.equal('Toggle navigation');
+    expect(buttons[1].textContent).to.equal('Show My Devices');
+  });
+
+  it('it invokes a GET request when button is clicked', () => {
+
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -7,7 +7,7 @@ import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from '../src/reducers';
+// import reducers from '../src/reducers';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
@@ -17,13 +17,13 @@ const $ = _$(window);
 chaiJquery(chai, chai.util, $);
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+  // const componentInstance =  TestUtils.renderIntoDocument(
+  //   <Provider store={createStore(reducers, state)}>
+  //     <ComponentClass {...props} />
+  //   </Provider>
+  // );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  // return $(ReactDOM.findDOMNode(componentInstance));
 }
 
 $.fn.simulate = function(eventName, value) {
